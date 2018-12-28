e8ed2d61efc30a3665535c051d0d0b5df4d7a0b5
moondy <xjmdy21@gmail.com>
2018-12-28 14:43:20 +0900

    두번째 commit

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -6,11 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserDAO {
-	
+
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
+
 	public UserDAO() {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/YOUTUBE?serverTimezone=UTC&useSSL=false";
@@ -18,34 +18,33 @@ public class UserDAO {
 			String dbPassword = "mysql123";
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int login(String userID, String userPassword) {
 		String SQL = "SELECT userPassword FROM USER WHERE userID = ?";
-		
+
 		try {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				if(rs.getString(1).equals(userPassword)) {
-					return 1; //로그인 성공
-				}
-				else
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword)) {
+					return 1; // 로그인 성공
+				} else
 					return 0; // 비밀번호 불일치
-				
+
 			}
 			return -1; // 아이디가 없음
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -2; //데이터베이스 오류
+		return -2; // 데이터베이스 오류
 	}
-	
+
 	public int join(User user) {
 		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?)";
 		try {
@@ -55,13 +54,13 @@ public class UserDAO {
 			pstmt.setString(3, user.getUserName());
 			pstmt.setString(4, user.getUserGender());
 			pstmt.setString(5, user.getUserEmail());
-			
+
 			return pstmt.executeUpdate();
-			
-		} catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return -1; //데이터베이스 오류
+		return -1; // 데이터베이스 오류
 	}
 
 }
